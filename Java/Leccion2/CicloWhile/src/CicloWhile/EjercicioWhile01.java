17d7cbd9865e5f2f2ef82195aaba7f39ce9b2aa5
Florencia Oviedo <flor.oviedo2010@gmail.com>
2022-08-27 16:17:08 -0300

    Clases en java con break,continue y labels

--- Java/Leccion2/CicloWhile/src/CicloWhile/EjercicioWhile01.java
+++ Java/Leccion2/CicloWhile/src/CicloWhile/EjercicioWhile01.java
@@ -15,9 +15,21 @@ public class EjercicioWhile01 {
             System.out.println("contador = " + contador);
             contador++;
         }while(contador<7);
+        //Uso de las palabras break y continue junto con etiquetas labels
         
-        for(var contando = 0; contando < 7 ; contando++){
-            System.out.println("contando = " + contando);
+        for(var contando = 0; contando < 7 ; contando++){     
+            if(contando % 2 == 0){
+                System.out.println("contando = " + contando);
+                break ;
+            }
         }
+        inicio:
+        for(var contando = 0; contando < 7 ; contando++){
+            if(contando % 2 != 0){
+                continue inicio; // vamos a la siguiente iteracion
+            }
+            System.out.println("contando = " + contando);
+        }  
+        
     }
 }
