c935949d25600f756d9f82c26067cd2a30f784f0
Florencia Oviedo <flor.oviedo2010@gmail.com>
2022-09-30 13:49:48 -0300

    Comitteando las clases de JavaScript de Tipos de Datos
    En estas clases vimos los distintos tipos de datos en javascript y la ejecución en Quookka y desde el navegador a través de un documento html

---
+++ Javascript/leccion02/02-01-TiposDatos.js
@@ -0,0 +1,19 @@
+// Tipos de Datos en JavaScript
+/*
+comentarios de multiples lineas
+La sintaxis en lo que es comentarios 
+es muy similar a la de Java
+realmente diriamos que es identica
+*/
+var nombre = 'Ariel'; // Tipo Str
+console.log(nombre);
+
+var numero = 3000; // Tipo numerico
+console.log(numero);
+
+var objeto = {
+    nombre : "Ariel",
+    Apellido : "Betancud",
+    Telefono : "2614567893"
+}
+console.log(objeto);
