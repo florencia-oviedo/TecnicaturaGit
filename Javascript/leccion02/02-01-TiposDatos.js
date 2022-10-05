64531ba670d347f13321f2222b78e8a7f4580945
Florencia Oviedo <flor.oviedo2010@gmail.com>
2022-10-04 21:23:06 -0300

    Tipos de Datos en js Parte 2

--- Javascript/leccion02/02-01-TiposDatos.js
+++ Javascript/leccion02/02-01-TiposDatos.js
@@ -6,14 +6,40 @@ es muy similar a la de Java
 realmente diriamos que es identica
 */
 var nombre = 'Ariel'; // Tipo Str
-console.log(nombre);
-
-var numero = 3000; // Tipo numerico
+console.log(typeof nombre);
+nombre = 7;
+console.log(typeof nombre);
+nombre = 12.3;
+console.log(typeof nombre);
+// Tipo numérico
+var numero = 3000; 
 console.log(numero);
-
+// Tipo Object
 var objeto = {
     nombre : "Ariel",
     Apellido : "Betancud",
     Telefono : "2614567893"
 }
 console.log(objeto);
+
+// Tipo de dato boolean
+var bandera = true;
+console.log(bandera);
+
+// Tipo de dato Funcion
+function miFuncion(){}
+console.log(typeof miFuncion);
+
+// Tipo de dato symbol
+var simbolo = Symbol("Mi simbolo");
+console.log(typeof simbolo);
+
+// Tipo de datos clase
+class Persona{
+    constructor(nombre,apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+
+console.log(typeof Persona);
