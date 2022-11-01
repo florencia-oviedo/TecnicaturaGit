3ed7786b630ff42d1f56d02c68188233d3e4e1cb
Florencia Oviedo <flor.oviedo2010@gmail.com>
2022-11-01 16:35:52 -0300

    Clase 7 de JavaScript

---
+++ Javascript/leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -0,0 +1,18 @@
+// Ejercio para encontrar números pares e impares
+let parImpar = 10;
+if( parImpar % 2 == 0){
+    console.log("Es un número par");
+}
+else{
+    console.log("Es un número impar");
+}
+
+//Es mayor de edad
+
+let edad = 14, adulto = 18;
+if(edad >= adulto){
+    console.log("Usted es una persona adulta");
+}
+else{
+    console.log("Usted es una persona de menor de edad");
+}
