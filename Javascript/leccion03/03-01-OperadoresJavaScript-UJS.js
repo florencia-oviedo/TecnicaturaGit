dc31087968580f8126d306c74e8aef982432307a
Florencia Oviedo <flor.oviedo2010@gmail.com>
2022-11-08 15:28:29 -0300

    Operadores en JavaScript y ejercicios
    Ejercicio par impar,rango y es mayor de edada

--- Javascript/leccion03/03-01-OperadoresJavaScript-UJS.js
+++ Javascript/leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -8,11 +8,20 @@ else{
 }
 
 //Es mayor de edad
-
 let edad = 14, adulto = 18;
 if(edad >= adulto){
     console.log("Usted es una persona adulta");
 }
 else{
     console.log("Usted es una persona de menor de edad");
+}
+
+//Ejercicio: dentro de un rango
+let dentroRango = 10; //Aquí vamos a ir cambiando el valor
+let valMin = 0 , valMax = 10;
+if(dentroRango >= valMin && dentroRango <= valMax){
+    console.log("Está dentro del rango establecido");
+}
+else{
+    console.log("Esta fuera del rango establecido");
 }
