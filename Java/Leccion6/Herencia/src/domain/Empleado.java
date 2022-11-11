3a14d6b970c05c9b9a4c18c1c9807183cc0d2505
Florencia Oviedo <flor.oviedo2010@gmail.com>
2022-11-11 20:38:31 -0300

    Clase 10 sobrecarga de constructores y metodos

--- Java/Leccion6/Herencia/src/domain/Empleado.java
+++ Java/Leccion6/Herencia/src/domain/Empleado.java
@@ -6,10 +6,14 @@ public class Empleado extends Persona {
     private int IdEmpleado;
     private double sueldo;
     private static int contadorEmpleados; //Es para incrementar
-
-    public Empleado(String nombre, double sueldo) {
-        super(nombre);
+    //Constructores
+    public Empleado(){ //constructor 1
         this.IdEmpleado = ++ Empleado.contadorEmpleados;
+    }
+    public Empleado(String nombre, double sueldo) { //constructor 2
+        //super(nombre);
+        this(); // Estamos llamando desde aquí al constructor vacío(llamar a un constructor interno)
+        this.nombre = nombre;
         this.sueldo = sueldo;
     }
 
